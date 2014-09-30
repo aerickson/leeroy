125c508d4e7c5f8c649ee9c768d1b65904680508
Andrew Erickson <aje@ticketfly.com>
2014-09-30 11:59:31 -0700

    code cleanup

--- leeroy/jenkins.py
+++ leeroy/jenkins.py
@@ -2,32 +2,13 @@
 
 import logging
 import requests
-#
-# build_path = "/job/{job_name}/buildWithParameters"\
-#     "?GIT_BASE_REPO={git_base_repo}" \
-#     "&GIT_HEAD_REPO={git_head_repo}" \
-#     "&GIT_SHA1={git_sha1}" \
-#     "&GITHUB_URL={github_url}"
-#
 
-## make work with auth tokens from https://wiki.jenkins-ci.org/display/JENKINS/Build+Token+Root+Plugin
-#
-# buildByToken/buildWithParameters?job=RevolutionTest&token=TacoTuesday&Type=Mexican
-#
+## works with auth tokens from https://wiki.jenkins-ci.org/display/JENKINS/Build+Token+Root+Plugin
 build_path = "/buildByToken/buildWithParameters?job={job_name}&token={jenkins_token}" \
     "&GIT_BASE_REPO={git_base_repo}" \
     "&GIT_HEAD_REPO={git_head_repo}" \
     "&GIT_SHA1={git_sha1}" \
     "&GITHUB_URL={github_url}"
-
-
-def get_jenkins_auth(app, repo_config):
-    user = repo_config.get("jenkins_user",
-                           app.config["JENKINS_USER"])
-    password = repo_config.get("jenkins_password",
-                               app.config["JENKINS_PASSWORD"])
-
-    return user, password
 
 
 def get_jenkins_url(app, repo_config):
@@ -48,7 +29,6 @@ def schedule_build(app, repo_config, head_repo_name, sha, html_url):
                           jenkins_token=token)
 
     logging.debug("Requesting build from Jenkins: %s", url)
-    #response = requests.post(url, auth=get_jenkins_auth(app, repo_config))
     response = requests.post(url)
     logging.debug("Jenkins responded with status code %s",
                   response.status_code)
